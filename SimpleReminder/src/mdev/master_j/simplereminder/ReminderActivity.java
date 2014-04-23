fdcd7102933dc145b1c2697b98b7f286e6914380
Noname Sorry <noemail@sor.ry>
2014-04-23 18:36:26 +0400

    CustomAdapter is inner class for ReminderActivity now

--- SimpleReminder/src/mdev/master_j/simplereminder/ReminderActivity.java
+++ SimpleReminder/src/mdev/master_j/simplereminder/ReminderActivity.java
@@ -2,12 +2,18 @@ package mdev.master_j.simplereminder;
 
 import java.util.Calendar;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 public class ReminderActivity extends ActionBarActivity {
 
@@ -32,18 +38,21 @@ public class ReminderActivity extends ActionBarActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		if (savedInstanceState != null) {
+		if (savedInstanceState == null) {
+			SharedPreferences preferences = getPreferences(MODE_PRIVATE);
+			long savedTime = preferences.getLong(KEY_SAVED_TIME_MS, 0L);
+			if (savedTime != 0L) {
+				Calendar calendar = Calendar.getInstance();
+
+				updateFormSavedDateAndTime(calendar);
+			}
+		} else {
 			long savedTime = savedInstanceState.getLong(KEY_SAVED_TIME_MS);
 			if (savedTime != 0L) {
-				Calendar c = Calendar.getInstance();
-				c.setTimeInMillis(savedTime);
-				formYear = c.get(Calendar.YEAR);
-				formMonth = c.get(Calendar.MONTH);
-				formDay = c.get(Calendar.DAY_OF_MONTH);
-				formHour = c.get(Calendar.HOUR_OF_DAY);
-				formMinute = c.get(Calendar.MINUTE);
-				formDateSet = true;
-				formTimeSet = true;
+				Calendar calendar = Calendar.getInstance();
+				calendar.setTimeInMillis(savedTime);
+
+				updateFormSavedDateAndTime(calendar);
 			}
 		}
 
@@ -55,6 +64,18 @@ public class ReminderActivity extends ActionBarActivity {
 		adapter.add("");
 		lV.setAdapter(adapter);
 		lV.setOnItemClickListener(new OnFormItemClickListener());
+	}
+
+	@Override
+	protected void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+
+		Calendar formCalendar = getCalendarByForm();
+		if (formCalendar != null) {
+			long formTimeMillis = formCalendar.getTimeInMillis();
+
+			outState.putLong(KEY_SAVED_TIME_MS, formTimeMillis);
+		}
 	}
 
 	private class OnFormItemClickListener implements OnItemClickListener {
@@ -77,22 +98,81 @@ public class ReminderActivity extends ActionBarActivity {
 		}
 	}
 
-	private boolean isFormInputCorrect() {
-		if (!formDateSet || !formTimeSet)
-			return false;
+	private Calendar getCalendarByForm() {
+		if (!formDateSet || !formTimeSet) {
+			return null;
+		}
 
 		Calendar formCalendar = Calendar.getInstance();
+
 		formCalendar.set(Calendar.YEAR, formYear);
 		formCalendar.set(Calendar.MONTH, formMonth);
 		formCalendar.set(Calendar.DAY_OF_MONTH, formDay);
 		formCalendar.set(Calendar.HOUR_OF_DAY, formHour);
 		formCalendar.set(Calendar.MINUTE, formMinute);
 
+		return formCalendar;
+	}
+
+	private boolean isFormInputCorrect() {
+		if (!formDateSet || !formTimeSet) {
+			return false;
+		}
+
+		Calendar formCalendar = getCalendarByForm();
+
 		Calendar nowCalendar = Calendar.getInstance();
 
-		if (formCalendar.compareTo(nowCalendar) <= 0)
+		if (formCalendar.compareTo(nowCalendar) <= 0) {
 			return false;
+		}
 
 		return true;
+	}
+
+	private void updateFormSavedDateAndTime(Calendar calendar) {
+		formDateSet = true;
+		formTimeSet = true;
+
+		formYear = calendar.get(Calendar.YEAR);
+		formMonth = calendar.get(Calendar.MONTH);
+		formMonth = calendar.get(Calendar.DAY_OF_MONTH);
+		formHour = calendar.get(Calendar.HOUR_OF_DAY);
+		formMinute = calendar.get(Calendar.MINUTE);
+	}
+
+	class CustomAdapter extends ArrayAdapter<String> {
+		private final Context CONTEXT;
+
+		public CustomAdapter(Context context) {
+			super(context, R.layout.list_item);
+			CONTEXT = context;
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			LayoutInflater inflater = (LayoutInflater) CONTEXT.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+			View rowView = inflater.inflate(R.layout.list_item, parent, false);
+
+			TextView header = (TextView) rowView.findViewById(R.id.textview_header);
+			TextView footer = (TextView) rowView.findViewById(R.id.textview_footer);
+
+			switch (position) {
+			case ReminderActivity.INDEX_ITEM_TITLE:
+				header.setText(R.string.label_title);
+				break;
+			case ReminderActivity.INDEX_ITEM_DATE:
+				header.setText(R.string.label_date);
+				break;
+			case ReminderActivity.INDEX_ITEM_TIME:
+				header.setText(R.string.label_time);
+				break;
+			case ReminderActivity.INDEX_ITEM_DESCRIPTION:
+				header.setText(R.string.label_description);
+				break;
+			}
+
+			return rowView;
+		}
 	}
 }
