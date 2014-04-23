24cfbaef4ed67c617141d0280af3307a5b679dcb
Noname Sorry <noemail@sor.ry>
2014-04-23 17:04:36 +0400

    +CustomAdapter; +basic getView; minors

---
+++ SimpleReminder/src/mdev/master_j/simplereminder/CustomAdapter.java
@@ -0,0 +1,43 @@
+package mdev.master_j.simplereminder;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+public class CustomAdapter extends ArrayAdapter<String> {
+	private final Context CONTEXT;
+
+	public CustomAdapter(Context context) {
+		super(context, R.layout.list_item);
+		CONTEXT = context;
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		LayoutInflater inflater = (LayoutInflater) CONTEXT.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		View rowView = inflater.inflate(R.layout.list_item, parent, false);
+
+		TextView header = (TextView) rowView.findViewById(R.id.textview_header);
+		TextView footer = (TextView) rowView.findViewById(R.id.textview_footer);
+
+		switch (position) {
+		case 0:
+			header.setText(R.string.label_title);
+			break;
+		case 1:
+			header.setText(R.string.label_date);
+			break;
+		case 2:
+			header.setText(R.string.label_time);
+			break;
+		case 3:
+			header.setText(R.string.label_description);
+			break;
+		}
+
+		return rowView;
+	}
+}
--- SimpleReminder/src/mdev/master_j/simplereminder/ReminderActivity.java
+++ SimpleReminder/src/mdev/master_j/simplereminder/ReminderActivity.java
@@ -2,6 +2,7 @@ package mdev.master_j.simplereminder;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.widget.ListView;
 
 public class ReminderActivity extends ActionBarActivity {
 
@@ -10,6 +11,13 @@ public class ReminderActivity extends ActionBarActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
+		CustomAdapter adapter = new CustomAdapter(this);
+		ListView lV = (ListView) findViewById(R.id.listView1);
+		adapter.add("");
+		adapter.add("");
+		adapter.add("");
+		adapter.add("");
+		lV.setAdapter(adapter);
 	}
-	
+
 }
