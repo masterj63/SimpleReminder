d1f1830c3200b0db29d31540474586b21871df92
Noname Sorry <noemail@sor.ry>
2014-04-24 16:01:57 +0400

    +empty DescriptionPickerDialog

---
+++ SimpleReminder/src/mdev/master_j/simplereminder/DescriptionPickerDialog.java
@@ -0,0 +1,14 @@
+package mdev.master_j.simplereminder;
+
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.os.Bundle;
+
+public class DescriptionPickerDialog extends DialogFragment {
+
+	@Override
+	public Dialog onCreateDialog(Bundle savedInstanceState) {
+		return super.onCreateDialog(savedInstanceState);
+	}
+
+}
