4fde0de0443de51313ad536be19e5d29b4b4fee1
Noname Sorry <noemail@sor.ry>
2014-04-24 15:31:41 +0400

    +empty TitlePickerFragment

---
+++ SimpleReminder/src/mdev/master_j/simplereminder/TitlePickerFragment.java
@@ -0,0 +1,12 @@
+package mdev.master_j.simplereminder;
+
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.os.Bundle;
+
+public class TitlePickerFragment extends DialogFragment {
+	@Override
+	public Dialog onCreateDialog(Bundle savedInstanceState) {
+		return super.onCreateDialog(savedInstanceState);
+	}
+}
