e97df98d6ad0b32d5c8d25bc97d7866be47e0931
Noname Sorry <noemail@sor.ry>
2014-05-05 19:41:25 +0400

    wakelocks are done, the screen is lightened up

--- SimpleReminder/src/mdev/master_j/simplereminder/NotificatorActivity.java
+++ SimpleReminder/src/mdev/master_j/simplereminder/NotificatorActivity.java
@@ -6,6 +6,8 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.PowerManager;
+import android.os.PowerManager.WakeLock;
 import android.support.v4.app.NotificationCompat;
 
 public class NotificatorActivity extends Activity {
@@ -33,6 +35,10 @@ public class NotificatorActivity extends Activity {
 
 		NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 		notificationManager.notify(0, builder.build());
+
+		PowerManager pm = (PowerManager) getSystemService(POWER_SERVICE);
+		WakeLock wl = pm.newWakeLock(PowerManager.ACQUIRE_CAUSES_WAKEUP | PowerManager.FULL_WAKE_LOCK, "");
+		wl.acquire(5500);
 
 		finish();
 	}
